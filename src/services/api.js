87f44fec4ed3b7dbd927a5e1579b29f48faf43e0
ajay mishra <ajay7415.mishra@gmail.com>
2026-01-10 03:47:49 +0530

    so many fixes

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.DEV
-    ? 'http://localhost:5000/api'
-    : 'https://timelinebackend.onrender.com/api';
+const API_BASE_URL = 'https://timelinebackend.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
